bd74d511b5b826575d1e5f6d0df41fab31b8a2d3
Pragati Rathore <pragati.rathore@brovitect.com>
2023-08-19 13:24:57 +0530

    added

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,16 +1,19 @@
 import './About.css';
-
+import AboutImg from "../../asstes/about-img.png"
 
 function About() {
   return (
     <div className="about-box">
         <div className="container">
         <div className="left-box">
-      <img className="image" src="your-image-url.jpg" alt="Your Image" />
+      <img className="" src={AboutImg} alt="AboutImg" />
     </div>
     <div className="right-box">
+      <div className="box-1">
       <h2>About Us</h2>
-      <p>This is the about us paragraph.</p>
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. t has survived not only five centuries.</p>
+      </div>
+     
       <button>Button</button>
     </div>
 
